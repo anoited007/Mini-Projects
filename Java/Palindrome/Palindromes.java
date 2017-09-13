73ffe80b59c20e8b5a1f27a5c592d741bcc18e50
anoited007 <esackey15@alustudent.com>
2017-09-13 17:19:56 +0400

    Initial commit

---
+++ Java/Palindrome/Palindromes.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+/**
+ * This class checks if an integer or a string is palindrome or not.
+ */
+
+public class Palindromes{
+    /**
+    * This method checks if a String is a palindrome.
+    * @param input takes string as parameter. The method checks if this string is a palindrome or not.
+    * @return Returns true if the String is palindrome and false if it is not.
+    */
+    public boolean checkPalindromeString(String input){
+        
+        // this pattern [^A-Za-z0-9] means: match all characters that are NOT in the set 
+        // A to Z, a to z, and 0 to 9.
+        String newInput =input.replaceAll("[^A-Za-z0-9]", "");
+        char[] word = newInput.toLowerCase().toCharArray();
+	int wordLength = word.length-1; 
+	
+        for(int i = 0; i<word.length; i++,wordLength--){
+            if(word[i]!=(word[wordLength])){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * @param input takes an integer as parameter. 
+     * @return returns true if the integer is palindrome and false if it is not.
+     */
+    public boolean checkPalindromeNumber(int input){
+        String newInput= Integer.toString(input);
+        return checkPalindromeString(newInput); 
+    }
+}
