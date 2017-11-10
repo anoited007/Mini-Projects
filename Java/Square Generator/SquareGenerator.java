46bad64a51a3af1864366cbe0ba3401305d3f7d7
Emmanuel Sackey <esackey15@alustudent.com>
2017-11-10 15:14:42 +0400

    Create SquareGenerator.java

---
+++ Java/Square Generator/SquareGenerator.java
@@ -0,0 +1,53 @@
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+/** 
+This class creates a square with the side lenght the user will input.
+*/
+public class Program
+{
+	public static void main(String[] args) {
+	    
+	    Scanner input = new Scanner(System.in);
+	    int sideOfSquare = 0;
+	    
+	    try{
+	         sideOfSquare = input.nextInt();
+	    } catch(InputMismatchException e) {
+	        System.out.println("Please enter a valid number!");
+	    }
+	    
+	    if(sideOfSquare >= 2) {
+	    
+	    for(int i=0; i<sideOfSquare; i++){
+	        if(i==0 || i == sideOfSquare -1){
+	            for(int j=0; j<sideOfSquare; j++){
+	                if(j==sideOfSquare-1)
+	                System.out.println("* ");
+	                
+	                else
+	                System.out.print("* ");
+	            }
+	            
+	        }
+	        
+	        else {
+	             for(int j=0; j<sideOfSquare; j++){
+	                if(j==0)
+	                System.out.print("* ");
+	                
+	                else if(j==sideOfSquare-1)
+	                System.out.println("* ");
+	                
+	                else
+	                System.out.print("  ");
+	            }
+	        }
+	    } 
+	}
+	
+	else 
+	System.out.println("Please enter a number greater than 1.");    
+	}
+}
